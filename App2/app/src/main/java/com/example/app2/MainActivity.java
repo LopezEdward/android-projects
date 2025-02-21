5951c14e5751bad2d7ccb44525c0d2c5e64f607a
edward <imanolopez3103@gmail.com>
2025-02-21 14:49:56 -0500

    Creación de función para buscar los botones (ImageView) en un elemento padre

--- App2/app/src/main/java/com/example/app2/MainActivity.java
+++ App2/app/src/main/java/com/example/app2/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.app2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View[] buttons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,20 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+    }
+
+    private View[] getButtons (ViewGroup parent) {
+        int numberOfChildren = parent.getChildCount();
+        View[] buttons = new View[numberOfChildren];
+
+        for (int i = 0; i < numberOfChildren; i++) {
+            View view = parent.getChildAt(i);
+
+            if (view instanceof ImageView) buttons[i] = view;
+        }
+
+        return buttons;
     }
 }
