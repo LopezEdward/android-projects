58d2dee743ef36cfb9bc5eb77f47957973e52a24
edward <imanolopez3103@gmail.com>
2025-02-21 18:33:01 -0500

    Creación de Activity para Instagram

--- App2/app/src/main/java/com/example/app2/Instagram.java
+++ App2/app/src/main/java/com/example/app2/Instagram.java
@@ -1,6 +1,7 @@
 package com.example.app2;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +10,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Instagram extends AppCompatActivity {
+    // Static url for Instagram web site.
+    private static final String INSTAGRAM_URL = "https://instagram.com";
+    // Get the WebView element in the Activity
+    //private final WebView MAIN_WEB_VIEW = this.findViewById(R.id.seeInstagram);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_instagram);
+
+        // Set the URL to WebView in the activity
+        WebView vWeb = (WebView) findViewById(R.id.seeInstagram);
+        vWeb.loadUrl(INSTAGRAM_URL);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
