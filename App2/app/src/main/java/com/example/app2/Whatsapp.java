f91f0aed5e352d34557877abefc9443a57e0c99e
edward <imanolopez3103@gmail.com>
2025-02-21 18:54:13 -0500

    Creación de WebView para Whatsapp

--- App2/app/src/main/java/com/example/app2/Whatsapp.java
+++ App2/app/src/main/java/com/example/app2/Whatsapp.java
@@ -1,6 +1,7 @@
 package com.example.app2;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +10,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Whatsapp extends AppCompatActivity {
-
+    // Define const about URL WhatsApp Web
+    private static final String WHATSAPP_WEB_URL = "https://web.whatsapp.com/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_whatsapp);
+
+        // Set URL to WebView component in Activity
+        WebView wView = this.findViewById(R.id.seeWhatsapp);
+        wView.loadUrl(WHATSAPP_WEB_URL);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
