4b63661d1bf1d8b791764d736036dfa888538be9
edward <imanolopez3103@gmail.com>
2025-02-21 18:40:12 -0500

    Creación de Activity para WebView de Instagram

--- App2/app/src/main/java/com/example/app2/Facebook.java
+++ App2/app/src/main/java/com/example/app2/Facebook.java
@@ -1,6 +1,7 @@
 package com.example.app2;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +10,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Facebook extends AppCompatActivity {
+    // Define const about URL of Facebook
+    private static final String FACEBOOK_URL = "https://facebook.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_facebook);
+
+        // Set the URL to WebView component in Activity
+        WebView wView = this.findViewById(R.id.seeFacebook);
+        wView.loadUrl(FACEBOOK_URL);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
